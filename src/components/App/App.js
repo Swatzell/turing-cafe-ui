2e1d4dd9561ae535457e04b49c02c7c88d99a671
David Swatzell <thatsstrikethree@gmail.com>
2024-07-22 10:50:16 -0500

    add inital imports to app.js and change the main app wrapper from div to main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import React from 'react';
+import Reservations from '../Reservations/Reservations';
+import Form from '../Form/Form';
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
       <div className='resy-form'>
       </div>
       <div className='resy-container'>
       </div>
-    </div>
+    </main>
   );
 }
 
