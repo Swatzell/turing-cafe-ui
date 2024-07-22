edb0700e7ae49289571f03e926b78027cbe8bc3e
David Swatzell <thatsstrikethree@gmail.com>
2024-07-22 11:09:03 -0500

    add dummy data and card.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,13 +5,34 @@ import Form from '../Form/Form';
 import { useState } from 'react';
 
 function App() {
+  const dummyReservations = [
+    {
+    id: 1,
+    name: 'Christie',
+    date: '12/29',
+    time: '7:00',
+    number: 12,
+  },
+  {
+    id: 2,
+    name: 'Leta',
+    date: '4/5',
+    time: '7:00',
+    number: 2,
+  },
+  {
+    id: 3,
+    name: 'Pam',
+    date: '1/21',
+    time: '6:00',
+    number: 4,
+  },
+]
+  const [reservations, setReservations] = useState(dummyReservations);
   return (
     <main className="App">
       <h1 className='app-title'>Turing Cafe Reservations</h1>
-      <div className='resy-form'>
-      </div>
-      <div className='resy-container'>
-      </div>
+    <Reservations/>
     </main>
   );
 }
